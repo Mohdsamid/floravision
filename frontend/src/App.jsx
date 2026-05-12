a0a3b33be458b04e063f85a517dd0dedb15538d2
Mohd Samid 78 <mohdsamid78@gmail.com>
2026-05-12 20:22:44 +0000

    initiate The Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { House } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-gray-900 m-1.5 p-1.5 ">
+          <House></House>
+      </div>
+    </>
+  )
+}
+
+export default App
